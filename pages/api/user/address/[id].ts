61c0c7452c337ac1a50dc424b4fa5531f4273c1b
Ashish Mehra <sdashish.am@gmail.com>
2022-05-04 22:04:01 +0530

    Remove address

--- pages/api/user/address/[id].ts
+++ pages/api/user/address/[id].ts
@@ -1,68 +1,13 @@
-import { User } from "@prisma/client";
+import nc from "next-connect";
 import { NextApiResponse, NextApiRequest } from "next";
 
-import { checkRequestType, generateResponse, getUser } from "../../../../utils";
-import prisma from "../../../../utils/prisma";
-import { Address, RequestType } from "../../../../utils/types";
+import controller from "../../../../controllers/address";
+import { catchAsyncError, generateResponse } from "../../../../utils";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    checkRequestType("PATCH", req.method as RequestType, res);
-    let user = (await getUser(req)) as Partial<User>;
-    const addressId = req.query?.id as string;
-    const { body } = req;
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onNoMatch: (req, res) => generateResponse("405", `Request type ${req.method} is not allowed.`, res),
+})
+  .patch(catchAsyncError(controller.patchRequestHandler))
+  .delete(catchAsyncError(controller.deleteRequestHandler));
 
-    // ⚠️ User not authorised
-    if (!user) {
-      return generateResponse("401", "Please login to update address.", res);
-    }
-
-    // ⚠️ If address not exist
-    if (!body.address && !body.default) {
-      return generateResponse("400", "Please provide  data.", res);
-    }
-
-    // ⚠️ if addressId not in records
-    const existingAddresses = user?.addresses || [];
-    let addressIndex = -1;
-    const isAddressExist = user.addresses?.some((address, index) => {
-      const existingAddress = address as Address;
-      if (existingAddress.id === addressId) {
-        addressIndex = index;
-        return true;
-      }
-    });
-
-    if (!user?.addresses || !isAddressExist) {
-      return generateResponse("400", "Address id not associate with any address in system.", res);
-    }
-
-    // 🔥 Update address
-    if (body.default) {
-      existingAddresses.map((address) => {
-        const addr = address as Address;
-        if (addr.id === addressId) {
-          addr.default = true;
-        } else {
-          addr.default = false;
-        }
-      });
-    }
-
-    if (body.address) {
-      const lastAddressUpdate = existingAddresses[addressIndex] as Object;
-      existingAddresses[addressIndex] = { ...lastAddressUpdate, ...body.address };
-    }
-
-    user = await prisma.user.update({
-      where: { id: user.id },
-      data: { addresses: [...existingAddresses] },
-    });
-
-    delete user.password;
-
-    return generateResponse("400", "Address successfully updated.", res, { data: user });
-  } catch (err) {
-    return generateResponse("400", "Something went wrong.", res);
-  }
-};
+export default handler;
