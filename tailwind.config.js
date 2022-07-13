fc305a27f34186d3931597ed5d960a0df287f39b
Ashish Mehra <sdashish.am@gmail.com>
2022-07-13 11:49:13 +0530

    Tailwind setup for admin portal

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/admin/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
