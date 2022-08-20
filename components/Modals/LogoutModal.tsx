87d9fcf71ad2657bee37933d9c65e9d33218bc3c
Ashish Mehra <sdashish.am@gmail.com>
2022-08-20 22:47:52 +0530

    Logout api ~ done

--- components/Modals/LogoutModal.tsx
+++ components/Modals/LogoutModal.tsx
@@ -1,4 +1,5 @@
 import { useCallback } from "react";
+import { appFetch } from "../../utils/api";
 import Button from "../Button";
 import CenterModal from "./CenterModal";
 import { SideModalI } from "./SideModal";
@@ -6,7 +7,15 @@ import { SideModalI } from "./SideModal";
 interface LogoutModalI extends SideModalI {}
 
 const LogoutModal: React.FC<LogoutModalI> = ({ visible, onClose }) => {
-  const onLogout = useCallback(() => {}, []);
+  const onLogout = useCallback(async () => {
+    await appFetch("signout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    window.location.href = "/admin/login";
+  }, []);
   return (
     <CenterModal visible={visible} onClose={onClose}>
       <h2 className="text-center text-xl mb-5">Are you sure you want to logout?</h2>
